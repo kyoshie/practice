9f60843dd297b1139de083a687e899c5db5baf2d
Joshua Datinguinoo <157869727+kyoshie@users.noreply.github.com>
2025-03-11 01:55:42 +0000

    chore: use state counter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,9 @@
-import { Props } from "@/components/props";
+import { Counter } from "@/components/state";
 
 export default function Home() {
   return (
    <div className="flex justify-center items-center h-full w-full flex-col gap-3">
-    <Props 
-      name = "joshua" 
-      age = {13}
-      height={164}
-      weight={64}
-    />
-     <Props 
-      name = "joshua" 
-      age = {13}
-      height={164}
-      weight={64}
-    />
-   </div>
+    <Counter />
+     </div>
   );
 }
---
+++ src/components/state.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { useState } from "react";
+
+export function Counter() {
+    const [count, setCount] = useState(0)
+    return(
+        <>
+            <div>
+                <h1>Counter: {count}</h1>
+                <button onClick={() => setCount(count+1)}>Increase</button>
+                <button onClick={() => setCount(count-1)}>Decrease</button>
+                <button onClick={() => setCount(0)}>Reset</button>
+            </div>
+        </>
+    )
+}
